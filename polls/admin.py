2effa241e0bb9d39116a254778206ef001a0fb8b
Isaac Ayodeji <deejecks@live.com>
2015-02-03 12:33:49 +0100

    superuser created

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from polls.models import Question
+
 # Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+  fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
