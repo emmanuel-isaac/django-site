85923485dad4f5e2322b751293943c0ae10c3ba8
Isaac Ayodeji <deejecks@live.com>
2015-02-03 02:56:17 +0100

    Question model updated

--- polls/models.py
+++ polls/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Question(models.Model):
   question_text = models.CharField(max_length=200)
   pub_date = models.DateTimeField('date published')
+  def __str__(self):
+    return 'question_text: {}\npub_date: {}'.format(self.question_text, self.pub_date)
 
 class Choice(models.Model):
   question = models.ForeignKey(Question)
   choice_text = models.CharField(max_length=200)
+  def __str__(self):
+    return self.choice_text
   votes = models.IntegerField(default=0)
